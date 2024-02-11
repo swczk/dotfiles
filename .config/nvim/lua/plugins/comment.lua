ea0e98142deeabb036fc1af0f04f7b6ffeac35a4
Pedro J de O Sawczuk <89877096+swczk@users.noreply.github.com>
2024-02-11 19:23:16 -0300

    change: add vscode comment shortcut

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -2,6 +2,13 @@ return {
 	"numToStr/Comment.nvim",
 	opts = {
 		-- add any options here
+    padding = true,
+    toggler = {
+        ---Line-comment toggle keymap
+        line = '<C-/>',
+        ---Block-comment toggle keymap
+        block = 'gbc',
+    },
 	},
-	lazy = false,
+ lazy = false,
 }
