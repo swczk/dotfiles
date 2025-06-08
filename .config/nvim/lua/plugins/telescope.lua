c58512abd5462efc0c353e1fd28f2ffb20802560
Pedro Sawczuk <89877096+swczk@users.noreply.github.com>
2025-06-08 11:21:02 -0300

    changed: find_files keymap to <C-k>

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -4,7 +4,7 @@ return {
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
 		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>ff", builtin.find_files, { noremap = true, silent = true })
+		vim.keymap.set("n", "<C-k>", builtin.find_files, { noremap = true, silent = true })
 		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { noremap = true, silent = true })
 		vim.keymap.set("n", "<leader>fb", builtin.buffers, { noremap = true, silent = true })
 		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { noremap = true, silent = true })
